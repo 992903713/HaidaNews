ed4e3ea99942407485ef07db9acd2e43051c5571
992903713 <992903713@qq.com>
2017-05-06 18:21:32 +0800

    完善了新闻详情

--- HaidaNews/Classes/Others/Request/request.js
+++ HaidaNews/Classes/Others/Request/request.js
@@ -25,31 +25,12 @@ function passageModel(passage)
 	this.News_Title = passage.News_Title;
 	this.News_Author = passage.News_Author;
 	this.News_Content = passage.News_Content;
-//	//匹配图片
-//	var imgReg = /<img.*?(?:>|\/>)/gi;
-//	//匹配src属性 
-//	var srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i;
-//	var arr = this.News_Content.match(imgReg);
-//	if(arr !== null)
-//	{
-//		for (var i = 0; i < arr.length ; i++) {
-//			var src = arr[i].match(srcReg);
-//		}
-//	}
-//
-//	//此处留下一个bug，一篇文章可能有多张图片
-//	this.News_picture = (src == undefined)? null:src[1];
-//
-//	if(this.News_picture !== null && this.News_picture[1] == 'u')
-//	{
 	if(passage.bz4.length > 0)
 	{
 		this.News_picture = passageImageAdress + passage.bz4;
 	}else{
 		this.News_picture = null;
 	}
-	console.log(this.News_picture);
-//	}
 	this.News_Freind = passage.News_Freind;//这里json拼写错误
 	this.News_Link = passage.News_Link;
 	this.News_Click = passage.News_Click;
@@ -70,6 +51,7 @@ function passageModel(passage)
 	}else{
 		this.news_first_pic = null;
 	}
+	console.log(this.news_first_pic);
 	this.Class_Name = passage.Class_Name;
 	this.sub_class_name = passage.sub_class_name;
 	this.news_second_class = passage.news_second_class;
